f2bac7fa484c671acdbf03b73e0679be655ad714
Tim Hemmer <themmer@d3banking.com>
2016-09-18 18:21:14 -0500

    Initial page example

---
+++ src/routes/home/home.js
@@ -0,0 +1,7 @@
+export class Home {
+  firstName = 'John';
+
+  //sayHello() {
+  //  alert(`Hello ${this.firstName} ${this.lastName}. Nice to meet you.`);
+  //}
+}
---
+++ src/routes/info/info.js
@@ -0,0 +1,7 @@
+export class Info {
+  firstName = 'John';
+
+  //sayHello() {
+  //  alert(`Hello ${this.firstName} ${this.lastName}. Nice to meet you.`);
+  //}
+}
